c9ba774333a1fb09150384d25072014da3462d15
Alec Cleofe <aleccleofe@gmail.com>
2024-01-14 14:20:38 -0600

    Implemented Initial Commands

--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 )
+
+func commandHelp() error {
+	fmt.Println("Available commands:")
+	fmt.Println("help - Shows this help message")
+	fmt.Println("exit - Exits the pokedex")
+	return nil
+}
+func commandExit() error {
+	os.Exit(0)
+	return nil
+}
 
 func main() {
 	type cliCommand struct {
@@ -12,18 +24,36 @@ func main() {
 		description string
 		callback    func() error
 	}
-	// Create a new scanner to read from standard input
+
+	commands := map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Shows a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exits the pokedex",
+			callback:    commandExit,
+		},
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 
 	fmt.Print("pokedex > ")
-
-	// Read input line by line
 	for scanner.Scan() {
-		text := scanner.Text() // Get the current line of text
+		text := scanner.Text()
 		if text == "" {
-			break // Exit loop if an empty line is entered
+			break
 		}
-		fmt.Println("You entered:", text)
+		command, ok := commands[text]
+		if !ok {
+			errors.New("Unknown command:" + text)
+			continue
+		}
+		command.callback()
+		fmt.Print("pokedex > ")
+
 	}
 
 	if err := scanner.Err(); err != nil {
