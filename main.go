a281b9b8a0f7411eae31d4b4a947e4bbed82ef75
Alec Cleofe <aleccleofe@gmail.com>
2024-01-14 14:08:17 -0600

    Add User Input Scanners

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
-package main 
+package main
 
-import ( 
+import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	type cliCommand struct {
+		name        string
+		description string
+		callback    func() error
+	}
+	// Create a new scanner to read from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("pokedex > ")
+
+	// Read input line by line
+	for scanner.Scan() {
+		text := scanner.Text() // Get the current line of text
+		if text == "" {
+			break // Exit loop if an empty line is entered
+		}
+		fmt.Println("You entered:", text)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error:", err)
+	}
+
 }
